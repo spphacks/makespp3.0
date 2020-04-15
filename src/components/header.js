c6d23aeedbadca70f80d231f3409367282eb46c2
Tomek B <tomek.botwicz1@gmail.com>
2020-04-14 21:31:49 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -145,8 +145,8 @@ const Header = ({ siteTitle }) => (
       <br / ><br />Learn to build & pitch a product with workshops, free food, and thousands in prizes.
     </SubTagline>
 
-    <Date>April 25th, 2020</Date>
-    <Link to="/register">
+    <Date>September 2020</Date>
+    {/*<Link to="/register">
       <Register> Register </Register>
     </Link>
     {/* <Link to="/live">
