116f41c825d60c5f15283fa19128aaee5a581d69
Tomek B <tomek.botwicz1@gmail.com>
2020-10-06 22:52:00 -0400

    Update countdown.js

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -90,7 +90,6 @@ const SVGCircle = ({ radius }) => (
     </svg>
 );
 
-// StackOverflow: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
     var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
 
@@ -123,7 +122,6 @@ function describeArc(x, y, radius, startAngle, endAngle) {
     return d;
 }
 
-// StackOverflow: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 function mapNumber(number, in_min, in_max, out_min, out_max) {
     return (
         ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
