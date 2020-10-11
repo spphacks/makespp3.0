c50de65105672c14459c1c232dc6f3d47a78e088
Tomek B <tomek.botwicz1@gmail.com>
2020-10-11 12:23:04 -0400

    landing closing update

--- src/components/landing.js
+++ src/components/landing.js
@@ -778,7 +778,9 @@ const Landing = () => (
                 <span className="right">2-5 PM</span> Judging
               </item>
               <item>
-                <span className="right">5-6 PM</span> Closing Ceremony & Winners
+                <span className="right">5-6 PM</span>
+                <span classname="right"> <a href="https://us02web.zoom.us/j/83305769250?pwd=QlBtYjJvbzBwcExobTFjVTZtT0JoUT09" target="_blank" rel="noreferrer">
+                <ScheduleButton>Closing Ceremony & Winners</ScheduleButton></a></span>
               </item>
           </div>
         </Col>
