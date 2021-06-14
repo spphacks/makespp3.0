91735a8e4c4a5cca3c986b0d08d60fe2d20d8484
Tomek B <46640406+botwiczt@users.noreply.github.com>
2021-06-14 18:17:19 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFFFFF`,
+        theme_color: `#FFFFFF`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`
       }
