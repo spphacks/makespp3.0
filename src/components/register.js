73868ac6fe3fec62febaa0723d40f62cfe39b0d1
Tomek <46640406+botwiczt@users.noreply.github.com>
2022-01-25 21:23:07 -0500

    register form

--- src/components/register.js
+++ src/components/register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { navigate } from "gatsby";
-import { Wrap, RegisterButton, Input} from './styles';
+import { Wrapper1, RegisterButton, Input, Select, Label, SubLabel, Textarea} from './styles';
+import { Row, Col } from "react-grid-system";
 
 const updateByPropertyName = (propertyName, value) => () => ({
   [propertyName]: value
@@ -11,7 +12,18 @@ class Join extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: "",
       email: "",
+      school: "",
+      year: "",
+      gender: "",
+      skill_level: "",
+      why: "",
+      team: "",
+      dietary: "",
+      entrepreneurship: "",
+      shirt: "",
+      online: "",
       time: ""
     };
 
@@ -34,21 +46,245 @@ class Join extends Component {
   render() {
     return (
         <form onSubmit={this.handleSubmit}>
-          <Wrap>
-            <Input
-              type="text"
-              name="email"
-              placeholder="name@example.com"
-              value={this.state.email}
-              required
-              onChange={event =>
-                this.setState(
-                  updateByPropertyName("email", event.target.value)
-                )
-              }
-            />
-            <RegisterButton type="submit">submit</RegisterButton>
-          </Wrap>
+          <br/><br/><br/>
+          <Wrapper1>
+          <Row>
+            <Col sm={4}>
+              <Input
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                value={this.state.name}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("name", event.target.value)
+                  )
+                }
+              />
+            </Col>
+            <Col sm={4}>
+              <Input
+                type="text"
+                name="email"
+                placeholder="name@example.com"
+                value={this.state.email}
+                required
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("email", event.target.value)
+                  )
+                }
+              />
+            </Col>
+            <Col sm={4}>
+              <Input
+                type="text"
+                name="school"
+                placeholder="School"
+                value={this.state.school}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("school", event.target.value)
+                  )
+                }
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={4}>
+              <br />
+              <Select
+                name="year"
+                value={this.state.year}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("year", event.target.value)
+                  )
+                }
+              >
+                <option value="" disabled selected>
+                  Class Year
+                </option>
+                <option>Middle School</option>
+                <option>Freshman</option>
+                <option>Sophomore</option>
+                <option>Junior </option>
+                <option>Senior</option>
+              </Select>
+            </Col>
+            <Col sm={4}>
+              <br />
+              <Select
+                name="gender"
+                value={this.state.gender}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("gender", event.target.value)
+                  )
+                }
+              >
+                <option value="" disabled selected>
+                  Gender
+                </option>
+
+                <option>Male</option>
+                <option>Female</option>
+                <option>Non-binary</option>
+                <option>Prefer not to say</option>
+              </Select>
+            </Col>
+            <Col sm={4}>
+              <br />
+              <Select
+                name="skill_level"
+                value={this.state.skill_level}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("skill_level", event.target.value)
+                  )
+                }
+              >
+                <option value="" disabled selected>
+                  Hackathon Experience
+                </option>
+
+                <option>Beginner (never attended a hackathon before)</option>
+                <option>Intermediate (familiar with hackathons)</option>
+                <option>Expert (attended many hackathons before)</option>
+              </Select>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col sm={12}>
+              <Label>Why do you want to attend MakeSPP?</Label>
+              <SubLabel>
+                Please share a couple sentences on what you'd like to learn or
+                build at MakeSPP.
+              </SubLabel>
+              <br />
+              <Textarea
+                name="why"
+                type="text"
+                placeholder=""
+                value={this.state.why}
+                onChange={event =>
+                  this.setState(updateByPropertyName("why", event.target.value))
+                }
+              />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col sm={2} />
+            <Col sm={8}>
+              <SubLabel>
+                If you have a team, please enter their names here. Make sure
+                they sign up too! If you don't have a team yet, don't worry,
+                there will be a team formation session at the beginning of the
+                event.{" "}
+              </SubLabel>
+              <Input
+                type="text"
+                name="team"
+                placeholder="Team members"
+                value={this.state.team}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("team", event.target.value)
+                  )
+                }
+              />{" "}
+            </Col>
+            <Col sm={2} />
+          </Row>
+          <br />
+          <Row>
+            <Col sm={4} />
+
+            <Col sm={4}>
+              <SubLabel>
+                Are you interested in attending the entrepreneurship panel
+                (advice from CEOs/founders)?{" "}
+              </SubLabel>
+              <Select
+                name="entrepreneurship"
+                value={this.state.entrepreneurship}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("entrepreneurship", event.target.value)
+                  )
+                }
+              >
+                <option value="" disabled selected>
+                  Select option
+                </option>
+
+                <option>Yes</option>
+                <option>No</option>
+                <option>Maybe</option>
+              </Select>
+            </Col>
+            <Col sm={4} />
+          </Row>
+          <Row>
+            <Col sm={4}>
+              <SubLabel>Any dietary restrictions?</SubLabel>
+              <Input
+                type="text"
+                name="dietary"
+                placeholder=""
+                value={this.state.dietary}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("dietary", event.target.value)
+                  )
+                }
+              />{" "}
+            </Col>
+            <Col sm={4}>
+              <SubLabel>T-Shirt size</SubLabel>
+              <Select
+                name="shirt"
+                value={this.state.shirt}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("shirt", event.target.value)
+                  )
+                }
+              >
+                <option value="" disabled selected>
+                  Select Size (Adult)
+                </option>
+
+                <option>S</option>
+                <option>M</option>
+                <option>L</option>
+                <option>XL</option>
+              </Select>
+            </Col>
+
+            <Col sm={4}>
+              <SubLabel>Anything to share?</SubLabel>
+              <Input
+                name="online"
+                type="text"
+                placeholder="Website, GitHub, Devpost..."
+                value={this.state.online}
+                onChange={event =>
+                  this.setState(
+                    updateByPropertyName("online", event.target.value)
+                  )
+                }
+              />{" "}
+            </Col>
+          </Row>
+          <br />
+
+
+
+            <RegisterButton type="submit">Submit</RegisterButton>
+            </Wrapper1>
         </form>
     );
   }
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-export const Wrap = styled.div`
-  white-space: nowrap;
-`;
-
 export const RegisterButton = styled.button`
 display: inline-block;
   cursor: pointer;
@@ -37,6 +33,9 @@ display: inline-block;
   }
 `;
 
+export const Wrapper1 = styled.div`
+`;
+
 export const Wrapper = styled.div`
   text-align: center;
   position: fixed;
@@ -56,5 +55,63 @@ export const Details = styled.div`
     margin-left: auto;
     margin-right: auto;
     max-width: 500px;
+  }
+`;
+
+export const Select = styled.select`
+  font-size: 18px;
+  padding: 10px;
+  border: 2px solid #e4e4e4;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: 5px;
+  width: 100%;
+  margin: 10px;
+  transition: all 300ms ease hover;
+  &:focus {
+    border: 2px solid #ca043f;
+  }
+`;
+
+export const Submit = styled.button`
+  font-size: 24px;
+  padding: 10px;
+  border: 3px solid #ca043f;
+  background: #fff;
+  width: 30%;
+  margin: 20px;
+  border-radius: 5px;
+  transition: all 300ms ease;
+  &:hover {
+    border: 3px solid #ca043f;
+    background: #ca043f;
+    color: #e4e4e4;
+    cursor: pointer;
+  }
+`;
+
+export const Label = styled.p`
+  margin: 0 0 10px 0;
+  padding: 0;
+  font-size: 24px;
+`;
+
+export const SubLabel = styled.p`
+  margin: 0;
+  padding: 0;
+  font-size: 14px;
+`;
+
+export const Textarea = styled.textarea`
+  height: 100px;
+  padding: 10px;
+  border: 2px solid #e4e4e4;
+  border-radius: 5px;
+  transition: all 300ms ease hover;
+  width: 90%;
+  font-size: 18px;
+  &:focus {
+    border: 2px solid #ca043f;
   }
 `;
